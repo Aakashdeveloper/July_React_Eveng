0c88e0e9101879b7783050283c996bb003497bf8
aakashDeveloper <ahanda205@gmail.com>
2019-07-05 17:27:14 +0100

    a

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// to require lib module
+import React from 'react'
+import ReactDom from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from './Home';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Logical and html
+const App = () => {
+    return(
+        <div>
+            <Home/>
+            <hr/>
+            <h1>My First React App</h1>
+            <h2>This is Second Tag</h2>
+            <div>
+                <h1>This is from second div</h1>
+            </div>
+        </div>
+    )
+}
+
+ReactDom.render(<App/>, document.getElementById('root'))
+
+
+
+// To make it availabe outside
