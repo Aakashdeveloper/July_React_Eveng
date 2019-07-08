9f010f99c9f440004ba117c281d3086633230198
aakashDeveloper <ahanda205@gmail.com>
2019-07-08 17:22:14 +0100

    a

---
+++ firstapp/src/components/Header.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render(){
+        return(
+            <header>
+                <div className="logo">Logo</div>
+                <center>
+                    <input/>
+                </center>
+            </header>
+        )
+    }
+}
+
+export default Header;
+
+
+
--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -2,14 +2,16 @@
 import React from 'react'
 import ReactDom from 'react-dom';
 
-import Home from './Home';
+import Home from './components/Home';
+import Header from './components/Header'
 
 // Logical and html
 const App = () => {
     return(
         <div>
-            <Home/>
+            <Header/>
             <hr/>
+            <Home/>
             <h1>My First React App</h1>
             <h2>This is Second Tag</h2>
             <div>
