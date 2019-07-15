d4f92534b61479533a8ebf482e229a9cfdf5f82a
aakashDeveloper <ahanda205@gmail.com>
2019-07-15 17:09:34 +0100

    a

---
+++ secondapp/src/component/Routing.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import { BrowserRouter, Route, Link} from 'react-router-dom';
+
+import Posts from './Posts';
+import Profile from './Profile';
+import Home from './Home';
+
+class Routing extends Component{
+    render(){
+        return(
+            <BrowserRouter>
+                <div>
+                <nav class="navbar navbar-inverse">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>                        
+                        </button>
+                        <a class="navbar-brand" href="#">NareshIT</a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/posts">Posts</Link></li>
+                            <li><Link to="/profile">Profile</Link></li>
+                        </ul>
+                        </div>
+                    </div>
+                    </nav>
+                    <Route exact path="/" component={Home}></Route>
+                    <Route path="/posts" component={Posts}></Route>
+                    <Route path="/profile" component={Profile}></Route>
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
+
+
+export default Routing;
